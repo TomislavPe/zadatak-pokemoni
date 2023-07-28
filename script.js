87821233ec86a089bd0e92e5f2ca663dbc783c2e
TomislavPe <52789802+TomislavPetrovic@users.noreply.github.com>
2023-07-28 19:10:11 +0200

    changes made

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+let pokemon_species;
+
 const source = document.getElementById("entry-template").innerHTML;
 const template = Handlebars.compile(source);
 
-const context = { title: "My New Post", body: "This is my first post!", imgURL:"https://robohash.org/matej" };
-const html = template(context);
-
-console.log(html)
 
 document.querySelector("body").innerHTML = html
+
+ function fetchData() {
+    fetch('https://pokeapi.co/api/v2/pokemon-color/yellow')
+        .then(response => response.json())
+        .then(data => {
+
+
+            
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
